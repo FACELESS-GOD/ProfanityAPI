b516eb456bd8a548ea16aac30be5abfc24c60d71
unknown <ganorkarmohit551@gmail.com>
2024-12-03 19:57:26 +0530

    Added Middleware

--- Middleware/AppLevelMidddleware.js
+++ Middleware/AppLevelMidddleware.js
@@ -1,6 +1,19 @@
+const Zod = require("zod");
+
+const ValidateSchema = Zod.object({
+    FirstName:Zod.string(),
+    LastName:Zod.string(),
+    Profanity:Zod.string()
+});
 
 function AppLevelMiddleware(req, res, next)
 {
+    const validationResponse = ValidateSchema.safeParse(req.body);
+    if(validationResponse.success != true)
+    {
+        res.status(404);
+        res.json({"Response":" Invalid Request"});
+    }
     if(req.body.FirstName.length > 10)
     {
         res.status(404);
@@ -15,6 +28,7 @@ function AppLevelMiddleware(req, res, next)
     {
         next();
     }
+    
     
 }
 
